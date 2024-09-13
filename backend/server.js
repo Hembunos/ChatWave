9abc99e39b9275bbe4a01449755ff88ec4b954c5
Hembunos <hembunospro1428@gmail.com>
2024-09-13 08:59:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 // const path = require("path");
 
 dotenv.config();
-connectDB(); 
-const app = express(); 
+connectDB();
+const app = express();
 
 
 app.use(express.json()); // to accept json data
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatwave-885v.onrender.com",
     // credentials: true,
   },
 });
